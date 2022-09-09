ef9a91354999c27d74fbd24433ab3f5a1a6dc83b
Rafael Batista <maranata_rafa@hotmail.com>
2022-09-08 23:17:11 -0400

    add AppStorage

--- Fructus/View/StartButtonView.swift
+++ Fructus/View/StartButtonView.swift
@@ -1,27 +1,30 @@
 import SwiftUI
 
 struct StartButtonView: View {
+    @AppStorage("isOnboarding") var isOnboarding: Bool?
+
     var body: some View {
-        Button(action:{
-            print("Exit the onboarding")
-        }){
+        Button(action: {
+            isOnboarding = false
+        }) {
             HStack(spacing: 8) {
                 Text("Start")
-                Image(systemName:EIcons.arrow_right_circle.rawValue)
-                    .imageScale(.large)
+                Image(systemName: EIcons.arrow_right_circle.rawValue)
+                        .imageScale(.large)
             }
-            .padding(.horizontal, 16)
-            .padding(.vertical, 10)
-            .background(
-                Capsule().strokeBorder(.white, lineWidth: 1.25))
-        }.accentColor(.white)
+                    .padding(.horizontal, 16)
+                    .padding(.vertical, 10)
+                    .background(
+                            Capsule().strokeBorder(.white, lineWidth: 1.25))
+        }
+                .accentColor(.white)
     }
 }
 
 struct StartButtonView_Previews: PreviewProvider {
     static var previews: some View {
         StartButtonView()
-            .preferredColorScheme(.dark)
-            .previewLayout(.sizeThatFits)
+                .preferredColorScheme(.dark)
+                .previewLayout(.sizeThatFits)
     }
 }
