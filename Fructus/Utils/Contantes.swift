e3f95ad9655415225e1283b899bae54f928e564b
Rafael Batista <maranata_rafa@hotmail.com>
2022-09-08 21:38:55 -0400

    remove constants unusing

--- Fructus/Utils/Contantes.swift
+++ Fructus/Utils/Contantes.swift
@@ -1,15 +1,5 @@
 import Foundation
 
-enum EFruits: String {
-    case blueberry = "blueberry"
-    case strawberry = "Strawberry"
-}
-
-enum EColors: String {
-   case blueberryLight = "ColorBlueberryLight"
-   case blueberryDark = "ColorBlueberryDark"
-}
-
 enum EIcons: String {
     case arrow_right_circle = "arrow.right.circle"
 }
